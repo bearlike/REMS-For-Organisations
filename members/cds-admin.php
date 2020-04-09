06f16b391a04b9095d55f0d389e6bb103892324e
Mahalakshumi <mahav2000@gmail.com>
2020-04-09 16:05:55 +0530

    Added Empty certificate template and new certificate structure

--- members/cds-admin.php
+++ members/cds-admin.php
@@ -2,9 +2,9 @@
 <?php 
 include("header.php");
 /* Directory Path Variables START */
-$Generated_Certificate = '../../public/Generated Certificate/';
+$Generated_Certificate = '../public/Generated Certificate/';
 $Uploaded_Files = "CDS_Admin/Uploaded files/";
-$Certificate_Template = "CDS_Admin/Certificate Templates/Participation Certificate.png";
+$Certificate_Template = "CDS_Admin/Certificate Templates/Empty Certificate.png";
 $Fonts_Path = "CDS_Admin/Fonts/";
 /* Directory Path Variables END */
 ?>
@@ -111,6 +111,11 @@ $Fonts_Path = "CDS_Admin/Fonts/";
                 <div class="" style="padding-bottom: 19px;">
                 <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post" enctype="multipart/form-data">
                     <input type="file" name="file" id="file" />
+                    <br>
+                    Event Name:<input type="text" name="event_name"/>
+                    <br>
+                    Date:<input type="text" name="date"/>
+                    <br>
                     <input class="btn btn-primary" type="submit" name="submit" />
                 </form>
                 </div>
@@ -181,43 +186,64 @@ $Fonts_Path = "CDS_Admin/Fonts/";
                                 </thead>
                                 <tbody>
 <?php
-    if (isset($storagename) && $handle = fopen($Uploaded_Files . $_FILES["file"]["name"], "r")) {
-        while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-            if ($flag) {
-                $flag = false;
-                continue;
-            }
-            /* Event variables START*/
-            $participant_name = ucwords($data[0]);
-            $registration_number = $data[1];
-            $department = ucwords($data[2]);
-            $year = $data[3];
-            $section = ucwords($data[4]);
-            $position = ucwords($data[5]);
-            $event_name = ucwords($data[6]);
-            $event_date = '3rd March';
-            /* Event variables END*/
-            $im = imagecreatefrompng($Certificate_Template);
-            $font_light = $Fonts_Path.'Raleway/Raleway-Light.ttf';
-            $font_regular = $Fonts_Path.'Raleway/Raleway-Regular.ttf';
-            imagettftext($im, 120, 0, 224, 1327, 0x3e4246, realpath($font_light), $participant_name);
-            imagettftext($im, 45, 0, 929, 1493, 0x3e4246, realpath($font_regular), $event_name);
-            imagettftext($im, 45, 0, 1035, 1685, 0x3e4246, realpath($font_regular), $event_date);
-            imagepng($im, $Generated_Certificate . $foldername[0] . '/Certificate-' . str_replace(" ","_",$event_name) . "_" . str_replace(" ","_",$participant_name) . "_" . $participant_id . '.png');
-            $cert_link = $Generated_Certificate . $foldername[0] . '/Certificate-' . str_replace(" ","_",$event_name) . "_" . str_replace(" ","_",$participant_name) . "_" . $participant_id . '.png';
-            imagedestroy($im);
-            $participant_id++;
-            $submit_sql = "INSERT INTO `certificates` (`name`,`regno`,`dept`,`year`,`section`,`position`,`cert_link`,`event_name`) VALUES ('" . $participant_name . "','" . $registration_number . "','" . $department . "','" . $year . "','" . $section . "','" . $position . "','" . $cert_link . "','" . $event_name . "');";
-            $submit_stmt = $conn->prepare($submit_sql);
-            if (!$submit_stmt) {
-                echo "Prepare failed: (" . $conn->errno . ") " . $conn->error . "<br>";
-            }
-            $submit_stmt->execute();
-            echo '<tr><td>' . $participant_id . ' </td><td> ' . $participant_name . '</td><td> ' . $registration_number . '</td><td> ' . $position . '</td><td> ' . $event_name . '</td><td> <a href="' . $cert_link . '">Link</a></td></tr>';
-            $submit_stmt->close();
-        }
-        fclose($handle);
-    }
+	if (isset($_POST["submit"])){
+		$im_template = imagecreatefrompng($Certificate_Template);
+		$font_light = $Fonts_Path.'Raleway/Raleway-Light.ttf';
+		$font_regular = $Fonts_Path.'Raleway/Raleway-Regular.ttf';
+		$font_semibold = $Fonts_Path.'Raleway/Raleway-SemiBold.ttf';
+		$font_medium =$Fonts_Path.'Raleway/Raleway-Medium.ttf';
+		$event_name_main = $_POST["event_name"];
+		$date_main = $_POST["date"];
+		imagettftext($im_template, 50, 0, 206, 1019, 0x535453, realpath($font_light), "This certificate is presented to");
+		imagettftext($im_template, 50, 0, 206, 1317,0x535453, realpath($font_light), "For participating in");
+		imagettftext($im_template, 60, 0, 206, 1439, 0x535453, realpath($font_medium), $event_name_main);
+		imagettftext($im_template, 55, 0, 206, 1559, 0x535453, realpath($font_light), "Conducted by");
+		imagettftext($im_template, 55, 0, 706, 1562, 0x535453, realpath($font_medium), "SVCE ACM Student Chapter");
+		imagettftext($im_template, 55, 0, 206, 1672, 0x535453, realpath($font_light), "on");
+		imagettftext($im_template, 55, 0, 315, 1672, 0x535453, realpath($font_medium), $date_main);
+		$generated_template =  $Generated_Certificate . $foldername[0] . '/template.png';
+		imagepng($im_template,$generated_template);
+		imagedestroy($im_template);
+
+	    if (isset($storagename) && $handle = fopen($Uploaded_Files . $_FILES["file"]["name"], "r")) {
+	        while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+	            if ($flag) {
+	                $flag = false;
+	                continue;
+	            }
+	            /* Event variables START*/
+	            $participant_name = ucwords($data[0]);
+	            $registration_number = $data[1];
+	            $department = ucwords($data[2]);
+	            $year = $data[3];
+	            $section = ucwords($data[4]);
+	            $position = ucwords($data[5]);
+	            $event_name = ucwords($data[6]);
+	            $email = ucwords($data[7]);
+	            /* Event variables END*/
+	            $im = imagecreatefrompng($generated_template);
+	           if(strlen($participant_name)>22){
+				imagettftext($im, 100-((strlen($name)-24)*7), 0, 206, 1185, 0x535453, realpath($font_medium), $participant_name);
+				}
+				else{
+					imagettftext($im, 100, 0, 206, 1185, 0x535453, realpath($font_medium), $participant_name);
+				}
+	            imagepng($im, $Generated_Certificate . $foldername[0] . '/Certificate-' . str_replace(" ","_",$event_name) . "_" . str_replace(" ","_",$participant_name) . "_" . $participant_id . '.png');
+	            $cert_link = $Generated_Certificate . $foldername[0] . '/Certificate-' . str_replace(" ","_",$event_name) . "_" . str_replace(" ","_",$participant_name) . "_" . $participant_id . '.png';
+	            imagedestroy($im);
+	            $participant_id++;
+	            $submit_sql = "INSERT INTO `certificates` (`name`,`regno`,`dept`,`year`,`section`,`position`,`cert_link`,`event_name`,`email`) VALUES ('" . $participant_name . "','" . $registration_number . "','" . $department . "','" . $year . "','" . $section . "','" . $position . "','" . $cert_link . "','" . $event_name . "','".$email."');";
+	            $submit_stmt = $conn->prepare($submit_sql);
+	            if (!$submit_stmt) {
+	                echo "Prepare failed: (" . $conn->errno . ") " . $conn->error . "<br>";
+	            }
+	            $submit_stmt->execute();
+	            echo '<tr><td>' . $participant_id . ' </td><td> ' . $participant_name . '</td><td> ' . $registration_number . '</td><td> ' . $position . '</td><td> ' . $event_name . '</td><td> <a href="' . $cert_link . '">Link</a></td></tr>';
+	            $submit_stmt->close();
+	        }
+	        fclose($handle);
+	    }
+	}
 ?>
                                 </tbody>
                             </table>
