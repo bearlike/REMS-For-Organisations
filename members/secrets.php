c4b21b7951411a7e259eaacd305e41bb7535baaf
Krishnakanth Alagiri <39209037+bearlike@users.noreply.github.com>
2020-04-15 14:13:43 +0530

    Modify Header to add Secrets

---
+++ members/secrets.php
@@ -0,0 +1,10 @@
+<?php
+     /* Sample Values Filled */
+     $servername = 'localhost';
+     $username = 'root';           
+     $password = '';
+     $dbname = 'svcehost_cms';
+     $formDB="svcehost_forms";
+     /* API Keys */
+     $shortcm_authorization = "ABCDEFABCDEFABCDEFABCDEF";
+?>
