dc31f90bc10b610c5b1d14577d11205cea2a81ae
Krishnakanth Alagiri <39209037+bearlike@users.noreply.github.com>
2020-04-18 16:18:41 +0530

    Updated Documentation

--- docs/files/Sample_CMS_Database.sql
+++ docs/files/Sample_CMS_Database.sql
@@ -13,24 +13,19 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 DELIMITER $$
 --
 -- Procedures
 --
-CREATE DEFINER=`root`@`localhost` PROCEDURE `AddUser` (IN `pLogin` VARCHAR(40), IN `pPassword` VARCHAR(255), IN `pName` VARCHAR(80), IN `pEmail` VARCHAR(255))  BEGIN
+CREATE PROCEDURE `AddUser` (IN `pLogin` VARCHAR(40), IN `pPassword` VARCHAR(255), IN `pName` VARCHAR(80), IN `pEmail` VARCHAR(255))  BEGIN
 	INSERT INTO login (LoginName, PasswordHash, FullName, Email) VALUES(pLogin, SHA(pPassword), pName, pEmail);
 END$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `CheckUser` (IN `pUser` VARCHAR(255), IN `pPassword` VARCHAR(255))  BEGIN
+CREATE PROCEDURE `CheckUser` (IN `pUser` VARCHAR(255), IN `pPassword` VARCHAR(255))  BEGIN
 	SELECT COUNT(*) as code FROM login WHERE LoginName=pUser && PasswordHash=SHA(pPassword);
 END$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `GetUser` (IN `pEmail` VARCHAR(255))  NO SQL
+CREATE PROCEDURE `GetUser` (IN `pEmail` VARCHAR(255))  NO SQL
 BEGIN
 SELECT LoginName as code from login where Email=pEmail;
 END$$
@@ -68,7 +63,7 @@ CREATE TABLE `events` (
   `id` int(255) NOT NULL,
   `event_name` varchar(255) NOT NULL,
   `date` date NOT NULL,
-  `isInter` BOOLEAN NOT NULL COMMENT 'True, if its an inter-college event'
+  `isInter` tinyint(1) NOT NULL COMMENT 'True, if its an inter-college event'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
@@ -79,11 +74,12 @@ CREATE TABLE `events` (
 --
 
 CREATE TABLE `login` (
-  `UserID` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `LoginName` varchar(40) NOT NULL,
   `PasswordHash` varchar(255) NOT NULL,
   `Email` varchar(255) NOT NULL,
-  `FullName` varchar(80) DEFAULT NULL
+  `FullName` varchar(80) DEFAULT NULL,
+  `IsAdmin`	tinyint(1) DEFAULT NULL  COMMENT 'TTrue, if user needs admin privileges'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -92,7 +88,7 @@ CREATE TABLE `login` (
 -- Password: admin
 --
 
-INSERT INTO `login` (`UserID`, `LoginName`, `PasswordHash`, `Email`, `FullName`) VALUES
+INSERT INTO `login` (`id`, `LoginName`, `PasswordHash`, `Email`, `FullName`) VALUES
 (1, 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'test@test.com', 'admin');
 
 -- --------------------------------------------------------
@@ -113,7 +109,7 @@ ALTER TABLE `events`
 -- Indexes for table `login`
 --
 ALTER TABLE `login`
-  ADD PRIMARY KEY (`UserID`),
+  ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `LoginName` (`LoginName`),
   ADD UNIQUE KEY `Email` (`Email`),
   ADD UNIQUE KEY `FullName` (`FullName`);
@@ -134,9 +130,6 @@ ALTER TABLE `events`
 -- AUTO_INCREMENT for table `login`
 --
 ALTER TABLE `login`
-  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
