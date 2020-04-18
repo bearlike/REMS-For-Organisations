791a4ed98c40832f74a42fb6948ee053e28aa9ba
Mahalakshumi <mahav2000@gmail.com>
2020-04-18 18:38:19 +0530

    Minor fixes to 404 page

--- members/404.php
+++ members/404.php
@@ -1,7 +1,7 @@
 <?php include("header.php");  ?>
-<html id="head_tag">
+<html>
 
-<head>
+<head id="head_tag">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>Page Not Found: SVCE-ACM</title>
