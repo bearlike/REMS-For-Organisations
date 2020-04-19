589f4bfa6994b664ee04715fa2c492e759af2602
K-Kraken <krishna.alagiri03@gmail.com>
2020-04-19 20:10:38 +0530

    Add Logging to Validation

--- members/validate.php
+++ members/validate.php
@@ -3,72 +3,88 @@
 * Username and Password Validator
 * For CMS For Organisations
 */
-session_start(); 
+session_start();
 if (!empty($_SESSION)) {
-    if($_SESSION['remember']==0){
-        session_destroy();
-        echo "no remember";
-        header('Location: member-login.php');
-    }
-    $_POST['uname']=$_SESSION['uname'];
-    $_POST['password']=$_SESSION['password'];
-    $_POST['remember']=$_SESSION['remember'];
+	if ($_SESSION['remember'] == 0) {
+		session_destroy();
+		echo "no remember";
+		header('Location: member-login.php');
+	}
+	$_POST['uname'] = $_SESSION['uname'];
+	$_POST['password'] = $_SESSION['password'];
+	$_POST['remember'] = $_SESSION['remember'];
 }
-if (!empty( $_POST)) {
+if (!empty($_POST)) {
     // Runtime Variables
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "svcehost_cms";
-    $uname = NULL;
-    /* Include Secret Keys such as APIs and override default database credentials */
-    include("secrets_.php");
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "svcehost_cms";
+	$uname = NULL;
+	/* Include Secret Keys such as APIs and override default database credentials */
+	include("secrets_.php");
 
     // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
+	$conn = new mysqli($servername, $username, $password, $dbname);
 
     // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
     // Call Procedures
-    $check_sql = "CALL CheckUser(\"".$_POST['uname']."\",\"".$_POST['password']."\");";
-    $result = $conn->query($check_sql);
-    foreach ($result as $row) {
-        if ($row["code"]>0) {
-            $flag=true;
-        }
-        else {
-            $flag=false;
-        }
-    }
-    
-    if ($flag==false) {       
-        $conn->close();     
-        echo "Unsuccessful";  
-        header('Location: member-login.php');
-    }
-    else {
-        $_SESSION['uname'] = $_POST['uname'];
-        $_SESSION['password'] = $_POST['password'];
-        if(empty(($_POST['remember']))){
-            $_POST['remember']=0;
-        }
-        if ($_POST['remember']==1){
-            $_SESSION['remember']=1;
-            echo "remembered";
-        } 
-        else{
-            $_SESSION['remember']=0;
-            echo "not remembered";
-        }
-        date_default_timezone_set('Asia/Kolkata');
-        $_SESSION['loginTime'] = date("Y-m-d H:i:s", time());
-        $conn->close();
-        header('Location: dashboard.php');
-    }
+	$check_sql = 'CALL CheckUser("' . $_POST['uname'] . '","' . $_POST['password'] . '");';
+	$result = $conn->query($check_sql);
+	foreach ($result as $row) {
+		if ($row["code"] > 0) {
+			$flag = true;
+		} else {
+			$flag = false;
+		}
+	}
+
+	if ($flag == false) {
+		$conn->close();
+		echo "Unsuccessful";
+		header('Location: member-login.php');
+	} else {
+		$_SESSION['uname'] = $_POST['uname'];
+		$_SESSION['password'] = $_POST['password'];
+		if (empty(($_POST['remember']))) {
+			$_POST['remember'] = 0;
+		}
+		if ($_POST['remember'] == 1) {
+			$_SESSION['remember'] = 1;
+			echo "remembered";
+		} else {
+			$_SESSION['remember'] = 0;
+			echo "not remembered";
+		}
+		date_default_timezone_set('Asia/Kolkata');
+		$_SESSION['loginTime'] = date("Y-m-d H:i:s", time());
+		$conn->close();
+		/* 
+        Needs Optimisation, currently restarting connection to flush 
+        previous result (to prevent "Commands out of sync; you can't 
+        run this command now" error) 
+        */
+        // Create connection
+		$conn = new mysqli($servername, $username, $password, $dbname);
+
+        // Check connection
+		if ($conn->connect_error) {
+			die("Connection failed: " . $conn->connect_error);
+		}
+		$Log = "Looged in Successfully from " . $_POST['ipadd'];
+		$LogSQL = 'CALL enterLog("' . $_POST['uname'] . '","' . $Log . '");';
+		echo $LogSQL;
+		if ($conn->query($LogSQL) === TRUE) {
+			echo "New record created successfully";
+		} else {
+			echo "Error: " . $conn->error;
+			header('Location: member-login.php');
+		}
+		header('Location: dashboard.php');
+	}
+} else {
+	header('Location: member-login.php');
 }
-else {
-    header('Location: member-login.php');
-}
-?>
