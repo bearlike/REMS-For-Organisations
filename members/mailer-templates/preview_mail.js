66346841d05c6ade27b7de35890ab4908ff517b3
Mahalakshumi <mahav2000@gmail.com>
2020-04-19 15:51:38 +0530

    Minor fixes to email template + Added global variable for link shortener

--- members/mailer-templates/preview_mail.js
+++ members/mailer-templates/preview_mail.js
@@ -758,24 +758,6 @@ PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DT
       <tr>
         <td>
           <div role="banner">
-            <div class="preheader"
-              style="Margin: 0 auto;max-width: 560px;min-width: 280px; width: 280px;width: calc(28000% - 167440px);">
-              <div style="border-collapse: collapse;display: table;width: 100%;">
-                <!--[if (mso)|(IE)]><table align="center" class="preheader" cellpadding="0" cellspacing="0" role="presentation"><tr><td style="width: 280px" valign="top"><![endif]-->
-                <div class="snippet"
-                  style="display: table-cell;Float: left;font-size: 12px;line-height: 19px;max-width: 280px;min-width: 140px; width: 140px;width: calc(14000% - 78120px);padding: 10px 0 5px 0;color: #adb3b9;font-family: sans-serif;">
-
-                </div>
-                <!--[if (mso)|(IE)]></td><td style="width: 280px" valign="top"><![endif]-->
-                <div class="webversion"
-                  style="display: table-cell;Float: left;font-size: 12px;line-height: 19px;max-width: 280px;min-width: 139px; width: 139px;width: calc(14100% - 78680px);padding: 10px 0 5px 0;text-align: right;color: #adb3b9;font-family: sans-serif;">
-                  <p style="Margin-top: 0;Margin-bottom: 0;">No images?
-                    <webversion style="text-decoration: underline;">Click here</webversion>
-                  </p>
-                </div>
-                <!--[if (mso)|(IE)]></td></tr></table><![endif]-->
-              </div>
-            </div>
             <div class="header"
               style="Margin: 0 auto;max-width: 600px;min-width: 320px; width: 320px;width: calc(28000% - 167400px);"
               id="emb-email-header-container">
