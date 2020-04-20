24c578c5624b785a508d7d320f139da78bdb3a5a
Krishnakanth Alagiri <39209037+bearlike@users.noreply.github.com>
2020-04-20 16:27:54 +0530

    Update Profile Picture according to logged in user

--- members/header.php
+++ members/header.php
@@ -26,6 +26,26 @@
             $logActivityConn->close();
       }
 
+      // function to retieve profile pic
+      // Call Example: retProfilePic($_SESSION['uname']); 
+      function retProfilePic($loggedOnUser) {
+            include("secrets_.php");
+            $loggedOnUser="admin";
+            $retProfilePic = new mysqli($servername, $username, $password, $dbname);
+            if ($retProfilePic->connect_error) {
+                  die("Connection failed: " . $retProfilePic->connect_error);
+            }
+            $retProfilePicSQL = 'select imgsrc from login where LoginName="' . $loggedOnUser . '" limit 1;';
+            // echo $retProfilePicSQL; // For Debugging
+            // echo "Successfully Logged"; // For Debugging
+            $logResults  = $retProfilePic->query($retProfilePicSQL);
+            foreach ($logResults as $row) {
+                  return $startPath . '/assets/img/avatars/users/' . $row["imgsrc"];          
+            }          
+            $retProfilePic->close();
+      }
+
+
       
       // If Session doesn't exist
       if (!$_SESSION['uname']){
