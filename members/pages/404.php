b2108c41cf6f02a933ad30bef568889d28c3a33d
K-Kraken <krishna.alagiri03@gmail.com>
2020-04-21 02:35:09 +0530

    Minor updates

--- members/pages/404.php
+++ members/pages/404.php
@@ -1,29 +1,29 @@
-<?php include("header.php");  ?>
+<?php include("../header.php");  ?>
 <html>
 
 <head id="head_tag">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>Page Not Found: SVCE-ACM</title>
-    <link rel="icon" type="image/png" sizes="600x600" href="../assets/img/Logo_White.png">
-    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
+    <link rel="icon" type="image/png" sizes="600x600" href="../../assets/img/Logo_White.png">
+    <link rel="stylesheet" href="../../assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.0/css/all.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="../assets/fonts/fontawesome5-overrides.min.css">
-    <link rel="stylesheet" href="../assets/css/custom.css">
+    <link rel="stylesheet" href="../../assets/fonts/fontawesome5-overrides.min.css">
+    <link rel="stylesheet" href="../../assets/css/custom.css">
 </head>
 
 <body id="page-top">
     <div id="wrapper">
-		<?php include("navigation.php"); ?>
+		<?php include("../navigation.php"); ?>
             <div class="container-fluid">
                 <div class="text-center mt-5">
                     <div class="error mx-auto" data-text="404">
                         <p class="m-0">404</p>
                     </div>
                     <p class="text-dark mb-5 lead">Page Not Found</p>
-                    <p class="text-black-50 mb-0">It looks like you found a glitch in the matrix...</p><a href="/">← Back to Dashboard</a></div>
+                    <p class="text-black-50 mb-0">It looks like you found a glitch in the matrix...</p><a href="../dashboard.php">← Back to Dashboard</a></div>
             </div>
         </div>
         <footer class="bg-white sticky-footer">
@@ -35,9 +35,9 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.bundle.min.js"></script>
-    <script src="../assets/js/bs-init.js"></script>
+    <script src="../../assets/js/bs-init.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.js"></script>
-    <script src="../assets/js/theme.js"></script>
+    <script src="../../assets/js/theme.js"></script>
 </body>
 
 </html>
