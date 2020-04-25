873071e835d3651830cea31afd155f71949fa7fc
Mahalakshumi <mahav2000@gmail.com>
2020-04-25 06:33:46 +0530

    Used SQL prepare to avoid SQL injections for cds-public.php

--- public/cds-public.php
+++ public/cds-public.php
@@ -37,31 +37,41 @@
             $countr = $rowc[0]; // Count total certificates
             $totalPages = ceil($countr/$perPage);
             $startPage = $perPage*($page-1);
-            $sql = "select event_name, date from events order by date limit ".$startPage.",".$perPage.";";
-            $result = $conn->query($sql);
+            $sql =  $conn->prepare("select event_name, date from events order by date limit :startpage , :perpage");
+            $sql->bindValue(':startpage', (int) $startPage, PDO::PARAM_INT);
+            $statement->bindValue(':perpage', (int) $perPage, PDO::PARAM_INT);
+            $result = $sql->execute();
         }
         // If user wants to see the cert generated and not the events conducted  [$mode=0]
         else{
             $mode=0;
             $event = strtolower($_GET['event']);
             // To know if an event exist or not
-            $sql = "select count(1) as code from events where event_name=\"".$event."\"";
-            $result = $conn->query($sql);
-            foreach ($result as $row) { 
+            $sql = $conn->prepare('select count(1) as code from events where event_name=?');
+            $sql->bind_param('s', $event);
+            $sql->execute();
+            $result = $sql->get_result();
+            foreach ($result as $row) {
                 if ($row["code"]==0) {
                     header('Location: ../index.php?status=notfound');
                 }
             }
             // To know if an event is inter or intra
-            $sql = "select isInter as code from events where event_name=\"".$event."\"";
-            $result = $conn->query($sql);
-            foreach ($result as $row) { 
+            $sql = $conn->prepare("select isInter as code from events where event_name=?");
+            $sql->bind_param('s', $event);
+            $sql->execute();
+            $result = $sql->get_result();
+            foreach ($result as $row) {
                 if ($row["code"]==0)
                     $isInter=False;
                 else
                     $isInter=True;
             }
-            $resultc = $conn->query("SELECT COUNT(DISTINCT name,regno,college,dept,year,section,position,cert_link) FROM certificates where event_name=\"".$event."\" AND name LIKE '%".$_GET['search']."%' ");
+            $search_value = '%'.$_GET['search'].'%';
+            $sql = $conn->prepare('SELECT COUNT(DISTINCT name,regno,college,dept,year,section,position,cert_link) FROM certificates where event_name=? AND name LIKE ? ');
+            $sql->bind_param("ss", $event,$search_value);
+            $sql->execute();
+            $resultc = $sql->get_result();
             $rowc = $resultc->fetch_row();
             $countr = $rowc[0]; // Count total certificates
             // calculate number of pages needed
@@ -69,12 +79,18 @@
             // Find the starting element for the current $page
             $startPage = $perPage*($page-1);
             // SLECT for table
-            if (!($isInter))
-                $sql = "SELECT DISTINCT name,regno,dept,year,section,position,cert_link from certificates where event_name=\"".$event."\" AND name LIKE '%".$_GET['search']."%' order by name limit ".$startPage.",".$perPage.";";            
-            else
-                $sql = "SELECT DISTINCT name,college,year,position,cert_link from certificates where event_name=\"".$event."\" AND name LIKE '%".$_GET['search']."%' order by name limit ".$startPage.",".$perPage.";";            
-            $result = $conn->query($sql);
-            // echo $countr; 
+            if (!($isInter)){
+                //$sql = "SELECT DISTINCT name,regno,dept,year,section,position,cert_link from certificates where event_name=\"".$event."\" AND name LIKE '%".$_GET['search']."%' order by name limit ".$startPage.",".$perPage.";";
+                $sql = $conn->prepare("SELECT DISTINCT name,regno,dept,year,section,position,cert_link from certificates where event_name= ? AND name LIKE ? order by name limit ?,?;");
+                $sql->bind_param("ssii", $event,$search_value,$startPage,$perPage);
+            }
+            else{
+                $sql = $conn->prepare("SELECT DISTINCT name,college,year,position,cert_link from certificates where event_name=? AND name LIKE ? order by name limit ?,?;");
+                $sql->bind_param("ssii", $event,$search_value,$startPage,$perPage);
+            }
+            $sql->execute();
+            $result = $sql->get_result();
+            // echo $countr;
         }
     }
     else {
@@ -150,7 +166,7 @@
             <div class="container-fluid">
                 <h3 class="text-dark mb-4">Certificates Distrubution System (CDS)</h3>
                 <?php
-                    if (($countr == 0) && ($mode == 0)) { 
+                    if (($countr == 0) && ($mode == 0)) {
                         echo "<div class=\"card shadow mb-4\">
                             <div class=\"card-header py-3\">
                                 <h6 class=\"text-primary m-0 font-weight-bold\">".ucwords($event)."</h6>
@@ -177,7 +193,7 @@
                                         <input type="hidden" name="event" value="<?php echo $_GET['event']; ?>"/>
                                         <input type="hidden" name="page" value="1"/>
                                         <?php
-                                            if(isset($_GET['mode'])) 
+                                            if(isset($_GET['mode']))
                                                 echo '<input type="hidden" name="mode" value=""/>';
                                         ?>
                                         <select onchange="this.form.submit()" name="perPage" class="form-control form-control-sm custom-select custom-select-sm">
@@ -289,7 +305,7 @@
                                 </tfoot>
                             </table>
                         </div>
-                        
+
                         <div class="row">
                             <div class="col-md-6 align-self-center">
                                 <p id="dataTable_info" class="dataTables_info" role="status" aria-live="polite"></p>
@@ -301,15 +317,15 @@
                                         <input type="hidden" name="event" value="<?php echo $_GET['event']; ?>"/>
                                         <input type="hidden" name="perPage" value="<?php echo $perPage; ?>"/>
                                         <?php
-                                            // Generate buttons for choosing pages 
+                                            // Generate buttons for choosing pages
                                             for ($x=1; $x<=$totalPages; $x++){
-                                                if($x==$page){ 
+                                                if($x==$page){
                                                     echo '<li class="page-item active"><button name="page" value="'.$x.'" class="page-link">'.$x.'</button></li>';
                                                 }
                                                 else{
                                                     echo '<li class="page-item"><button name="page" value="'.$x.'" class="page-link">'.$x.'</button></li>';
                                                 }
-                                             } 
+                                             }
                                         ?>
 
                                         <li class="page-item <?php if($page==$totalPages){echo 'disabled';} ?>"><button name="page" value="<?php echo ($page+1); ?>" class="page-link" aria-label="Next"><span aria-hidden="true">»</span></button></li>
@@ -317,7 +333,7 @@
                                 </nav>
                             </form>
                         </div>
-                        
+
                     </div>
                 </div>
             </div>
@@ -343,12 +359,12 @@
         });
         var isMobile = false; //initiate as false
         // device detection
-        if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
-            || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) { 
+        if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
+            || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) {
             isMobile = true;
             document.getElementById('navbar').classList.add('toggled');
         }
-        
+
     </script>
 
 </body>
